20eeb504775e33e87caeabf5d9c10eb1dc389102
Kristoffer <fraithor@gmail.com>
2021-07-04 00:35:00 +0200

    Started animations between containers
    (Project X)
    
    -Kris

--- Script/index.js
+++ Script/index.js
@@ -1,19 +1,45 @@
 function setProjects() {
-    document.getElementById("default").style.display = 'none';
-    document.getElementById("projects").style.display = 'grid';
-    window.localStorage.setItem('lastOpen', 'projects');
+    if (window.localStorage.getItem('lastOpen') === 'projects') return;
+    document.querySelectorAll("#default > div:not(.navbar)").forEach(el => {
+        el.style.opacity = 0;
+    });
+    setTimeout(function(){
+        document.getElementById("default").style.display = 'none';
+        document.getElementById("projects").style.display = 'grid';
+        document.querySelector('title').textContent = "Projects - Vorlus";
+        window.localStorage.setItem('lastOpen', 'projects');
+        document.querySelectorAll("#projects > div:not(.navbar)").forEach(el => {
+            el.style.opacity = 1;
+        });
+    }, 500);
 }
 function setDefault() {
-    document.getElementById("default").style.display = 'grid';
-    document.getElementById("projects").style.display = 'none';
-    window.localStorage.setItem('lastOpen', 'default');
+    if (window.localStorage.getItem('lastOpen') === 'default') return;
+    document.querySelectorAll("#projects > div:not(.navbar)").forEach(el => {
+        el.style.opacity = 0;
+    });
+    setTimeout(function(){
+        document.getElementById("default").style.display = 'grid';
+        document.getElementById("projects").style.display = 'none';
+        document.querySelector('title').textContent = "Vorlus - Home";
+        window.localStorage.setItem('lastOpen', 'default');
+        document.querySelectorAll("#default > div:not(.navbar)").forEach(el => {
+            el.style.opacity = 1;
+        });
+    }, 500);
 }
 function prepare() {
     const lastOpen = localStorage.getItem("lastOpen");
     if (lastOpen === 'projects'){
-        setProjects()
+        document.getElementById("default").style.display = 'none';
+        document.getElementById("projects").style.display = 'grid';
     } else {
-        setDefault()
+        document.getElementById("default").style.display = 'grid';
+        document.getElementById("projects").style.display = 'none';
     }
 
 }
+
+// document.addEventListener('DOMContentLoaded', function(event) {
+//     document.querySelector('body').style.opacity = 1;
+// })
