eaf0288d59f8c0dbc28ecf7c51c97a5b49f3a9ea
Kristoffer <fraithor@gmail.com>
2021-07-04 20:33:21 +0200

    Complete page transition
    (Project X)
    
    -Kris

--- Script/index.js
+++ Script/index.js
@@ -1,31 +1,25 @@
 function setProjects() {
     if (window.localStorage.getItem('lastOpen') === 'projects') return;
-    document.querySelectorAll("#default > div:not(.navbar)").forEach(el => {
-        el.style.opacity = 0;
-    });
+    document.querySelector('#default').style.opacity = 0;
+    document.querySelector('#projects').style.opacity = 0;
     setTimeout(function(){
         document.getElementById("default").style.display = 'none';
         document.getElementById("projects").style.display = 'grid';
         document.querySelector('title').textContent = "Projects - Vorlus";
         window.localStorage.setItem('lastOpen', 'projects');
-        document.querySelectorAll("#projects > div:not(.navbar)").forEach(el => {
-            el.style.opacity = 1;
-        });
+        document.querySelector('#projects').style.opacity = 1;
     }, 500);
 }
 function setDefault() {
     if (window.localStorage.getItem('lastOpen') === 'default') return;
-    document.querySelectorAll("#projects > div:not(.navbar)").forEach(el => {
-        el.style.opacity = 0;
-    });
+    document.querySelector('#projects').style.opacity = 0;
+    document.querySelector('#default').style.opacity = 0;
     setTimeout(function(){
         document.getElementById("default").style.display = 'grid';
         document.getElementById("projects").style.display = 'none';
         document.querySelector('title').textContent = "Vorlus - Home";
         window.localStorage.setItem('lastOpen', 'default');
-        document.querySelectorAll("#default > div:not(.navbar)").forEach(el => {
-            el.style.opacity = 1;
-        });
+        document.querySelector('#default').style.opacity = 1;
     }, 500);
 }
 function prepare() {
@@ -40,6 +34,6 @@ function prepare() {
 
 }
 
-// document.addEventListener('DOMContentLoaded', function(event) {
-//     document.querySelector('body').style.opacity = 1;
-// })
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1;
+})
